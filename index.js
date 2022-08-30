27462240f5c16b469e38c0d87853644c6fcd57bf
moath al-abdulrazaq <moath@moaths-air.home>
2022-08-30 14:15:38 +0300

    6th

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors")
 dotenv.config(); 
 
 mongoose.connect(
@@ -15,10 +19,14 @@ mongoose.connect(
     console.log(err)
 }); 
 
+app.use(cors());
 app.use(express.json())
 app.use("/api/auth" , authRoute); 
 app.use("/api/users" , userRoute); 
 app.use("/api/products" , productRoute); 
+app.use("/api/carts" , cartRoute); 
+app.use("/api/orders" , orderRoute); 
+// app.use("/api/checkout" , stripeRoute);  
 
 
 
